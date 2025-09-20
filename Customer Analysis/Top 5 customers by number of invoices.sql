23a6a107313464bddc60020ea264c5dd688f1aff
Kristers Kalkis <45048232+astr0q@users.noreply.github.com>
2025-09-20 12:47:38 +0300

    Update Top 5 customers by number of invoices.sql

--- Customer Analysis/Top 5 customers by number of invoices.sql
+++ Customer Analysis/Top 5 customers by number of invoices.sql
@@ -1,1 +1,10 @@
-
+select 
+	concat(FirstName, ' ', LastName) as Name,
+    cu.CustomerId,
+    count(il.InvoiceId) as totalInvoices,
+    sum(total) as totalSum
+from customer cu
+join invoice il on cu.CustomerId = il.CustomerId
+group by cu.CustomerId, Name
+order by totalInvoices desc
+limit 5
