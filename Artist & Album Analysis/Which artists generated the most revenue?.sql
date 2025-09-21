7cb1607a6dec64b3d2306513c76ee844f3eb421f
Kristers Kalkis <45048232+astr0q@users.noreply.github.com>
2025-09-21 22:55:38 +0300

    Update Which artists generated the most revenue?.sql

--- Artist & Album Analysis/Which artists generated the most revenue?.sql
+++ Artist & Album Analysis/Which artists generated the most revenue?.sql
@@ -1,1 +1,10 @@
-
+use chinook;
+select 
+    ar.Name as namee,
+    sum(il.Quantity * il.UnitPrice) as total
+ from invoiceline il
+join track tr on il.TrackId = tr.TrackId
+join album ab on tr.AlbumId = ab.AlbumId
+JOIN Artist ar ON ab.ArtistId = ar.ArtistId
+group by  namee
+order by total desc
